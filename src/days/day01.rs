9d5f8080231279e2ab0c5dcf00d665955e66a04f
zendril <kenneth.s.brooks@gmail.com>
2022-12-02 15:40:06 -0800

    Day 1 complete!

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,25 +1,53 @@
-use nom::IResult;
+use itertools::Itertools;
+use nom::{character::complete::{line_ending, u32},
+          combinator::map,
+          IResult,
+          multi::{count, separated_list0},
+};
 
 use crate::days::Day;
 
 pub struct Day01;
 
 impl Day for Day01 {
-    type Input = String;
+    type Input = Vec<Vec<usize>>;
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        separated_list0(
+            count(line_ending, 2),
+            separated_list0(line_ending, map(u32, |c| c as usize)),
+        )(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        let mut high_elf = 0;
+        let mut high_elf_calories = 0;
+        for (i, x) in input.iter().enumerate() {
+            //println!("Item {} = {:?}", i, x);
+            let total = x.iter().sum();
+            // println!("{total}");
+            if total > high_elf_calories {
+                high_elf_calories = total;
+                high_elf = i;
+                // println!("{} {}", high_elf, high_elf_calories);
+            }
+        }
+        high_elf_calories
     }
 
     type Output2 = usize;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        // method 1
+        // let mut totals: Vec<usize> = input.iter().map(|inner| { inner.iter().sum() }).collect::<Vec<usize>>();
+        // totals.sort();
+        // totals.reverse();
+        // *&totals[0..3].iter().sum::<usize>()
+
+        // method 2
+        input.iter().map(|inner| { inner.iter().sum::<usize>() }).sorted().rev().take(3).sum()
+
     }
 }
